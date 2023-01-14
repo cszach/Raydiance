7988741e09a684873f71890a351a6c138dc4f85c
Duong (Zach) Nguyen <novak@novakcgx.me>
2023-01-14 11:51:37 -0500

    Renderer: rudimentary ray collision

--- include/3d/Renderer.hpp
+++ include/3d/Renderer.hpp
@@ -3,34 +3,50 @@
 
 #include "Camera.hpp"
 #include "ImageOutput.hpp"
+#include "MathUtils.hpp"
+#include "Ray.hpp"
 #include "Scene.hpp"
-
-using std::shared_ptr;
+#include "Vec3.hpp"
 
 class Renderer {
 private:
-  shared_ptr<ImageOutput> output;
+  ImageOutput &output;
 
 public:
-  Renderer(shared_ptr<ImageOutput> output) : output(output) {}
+  Renderer(ImageOutput &output) : output(output) {}
 
-  shared_ptr<ImageOutput> getOutput() const { return this->output; }
+  ImageOutput &getOutput() const { return this->output; }
 
   void render(const Scene &scene, const Camera &camera) {
-    const int WIDTH = (*this->output).getWidth();
-    const int HEIGHT = (*this->output).getHeight();
+    const int WIDTH = this->output.getWidth();
+    const int HEIGHT = this->output.getHeight();
 
     for (int i = 0; i < WIDTH; i++) {
       for (int j = 0; j < HEIGHT; j++) {
-        int r =
-            static_cast<int>(255.999 * static_cast<double>(i) / (WIDTH - 1));
-        int g =
-            static_cast<int>(255.999 * static_cast<double>(j) / (HEIGHT - 1));
-        int b = static_cast<int>(255.99 * 0.25);
+        auto u = static_cast<double>(i) / (WIDTH - 1);
+        auto v = static_cast<double>(j) / (HEIGHT - 1);
 
-        (*this->output).writeColor(r, g, b);
+        Ray ray = camera.getRay(u, v);
+        Color ray_color = this->getRayColor(ray, scene);
+
+        int r = static_cast<int>(256 * clamp(ray_color.getX(), 0, 0.999));
+        int g = static_cast<int>(256 * clamp(ray_color.getY(), 0, 0.999));
+        int b = static_cast<int>(256 * clamp(ray_color.getZ(), 0, 0.999));
+
+        this->output.writeColor(r, g, b);
       }
     }
+  }
+
+private:
+  Color getRayColor(const Ray &ray, const Scene &scene) {
+    HitRecord rec;
+
+    if (scene.hit(ray, 0.001, INFINITY, rec)) {
+      return 0.5 * (rec.normal + Color(1, 1, 1));
+    }
+
+    return Color(0, 0, 0);
   }
 };
 
