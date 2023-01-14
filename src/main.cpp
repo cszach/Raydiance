a44938d999929f1953223ef422bd03e105c1c1e4
Duong (Zach) Nguyen <novak@novakcgx.me>
2023-01-14 11:51:52 -0500

    main: Test scene with a sphere

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,44 @@
 #include <fstream>
+#include <memory>
 #include <ostream>
 
 #include "PPMOutput.hpp"
 #include "Renderer.hpp"
+#include "Scene.hpp"
+#include "Sphere.hpp"
+
+using std::make_shared;
+using std::shared_ptr;
 
 int main() {
   // Image
 
   const int IMAGE_WIDTH = 256;
   const int IMAGE_HEIGHT = 256;
+  const int MAX_COLOR = 255;
 
   std::ofstream f_out("image.ppm");
   std::ostream &f_ostream(f_out);
 
   f_ostream.rdbuf(f_out.rdbuf());
 
-  auto pOutput =
-      make_shared<PPMOutput>(IMAGE_WIDTH, IMAGE_HEIGHT, 255, f_ostream);
+  auto output = PPMOutput(IMAGE_WIDTH, IMAGE_HEIGHT, MAX_COLOR, f_ostream);
 
-  pOutput->writeHeader();
+  output.writeHeader();
 
-  Renderer renderer(pOutput);
+  // Camera
 
-  renderer.render(Scene(), Camera());
+  Camera camera(100, 1);
+
+  // Scene
+
+  Scene scene;
+  shared_ptr<Sphere> test_sphere = make_shared<Sphere>(0.5);
+  test_sphere->setPosition(Point3(0, 0, -1));
+
+  scene.add(test_sphere);
+
+  Renderer renderer(output);
+
+  renderer.render(scene, camera);
 }
