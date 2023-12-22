243198a1831d795d2866f6b42d2813f4085c7a9e
Zach <duong.nguyen@rochester.edu>
2023-12-22 17:17:42 -0500

    [main]: Update main

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,20 @@ using std::shared_ptr;
 int main() {
   // Image
 
-  const int IMAGE_WIDTH = 256;
-  const int IMAGE_HEIGHT = 256;
+  const double ASPECT_RATIO = 16.0 / 9.0;
+  const int IMAGE_WIDTH = 400;
+  const auto IMAGE_HEIGHT = static_cast<int>(IMAGE_WIDTH / ASPECT_RATIO);
 
   std::ofstream f_out("image.ppm");
 
   // Camera
 
-  Camera camera(100, 1);
+  Camera camera(100, ASPECT_RATIO);
 
   // Scene
 
   Scene scene;
-  shared_ptr<Sphere> test_sphere = make_shared<Sphere>(0.5);
+  auto test_sphere = make_shared<Sphere>(0.5);
   test_sphere->setPosition(Point3(0, 0, -1));
 
   scene.add(test_sphere);
@@ -32,25 +33,24 @@ int main() {
 
   Renderer renderer(IMAGE_WIDTH, IMAGE_HEIGHT);
 
-  float *frame_buffer = renderer.getFrameBuffer();
-
   renderer.render(scene, camera);
 
   // Write to PPM
+  const std::vector<float> frame_buffer = renderer.getFrameBuffer();
 
   f_out << "P3\n" << IMAGE_WIDTH << ' ' << IMAGE_HEIGHT << '\n' << 255 << '\n';
 
-  for (int i = 0; i < IMAGE_WIDTH; i++) {
-    for (int j = 0; j < IMAGE_HEIGHT; j++) {
+  for (int j = 0; j < IMAGE_HEIGHT; ++j) {
+    for (int i = 0; i < IMAGE_WIDTH; ++i) {
       int pixel_index = 3 * (i + j * IMAGE_WIDTH);
 
       float r = frame_buffer[pixel_index + 0];
       float g = frame_buffer[pixel_index + 1];
       float b = frame_buffer[pixel_index + 2];
 
-      int ir = int(255.99 * r);
-      int ig = int(255.99 * g);
-      int ib = int(255.99 * b);
+      auto ir = int(255.99 * r);
+      auto ig = int(255.99 * g);
+      auto ib = int(255.99 * b);
 
       f_out << ir << ' ' << ig << ' ' << ib << '\n';
     }
