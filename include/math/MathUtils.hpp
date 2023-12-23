b0e86ff032b9bd1761d82fe752e107a7bee121f1
Zach <duong.nguyen@rochester.edu>
2023-12-23 16:41:39 -0500

    [MathUtils]: Add random number utilities

--- include/math/MathUtils.hpp
+++ include/math/MathUtils.hpp
@@ -2,6 +2,7 @@
 #define MATH_UTILS_H
 
 #include <limits>
+#include <random>
 
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
@@ -14,6 +15,20 @@ inline double clamp(double value, double min, double max) {
   if (value > max)
     return max;
   return value;
+}
+
+inline double random_double() {
+  static std::uniform_real_distribution distribution(0.0, 1.0);
+  static std::mt19937 generator;
+
+  return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+  static std::uniform_real_distribution distribution(min, max);
+  static std::mt19937 generator;
+
+  return distribution(generator);
 }
 
 #endif // MATH_UTILS_H
