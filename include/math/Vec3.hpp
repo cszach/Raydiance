0850117bf006556ec104844f5b6d17589381865d
Zach <duong.nguyen@rochester.edu>
2023-12-23 20:16:01 -0500

    [Vec3]: Update API
    - Attributes are now public
    - Remove trivial getters and setters
    - Dot, cross, and normalize are immutable methods
    - Add static methods for random vector generation

--- include/math/Vec3.hpp
+++ include/math/Vec3.hpp
@@ -3,72 +3,98 @@
 
 #include <cmath>
 
-class Vec3 {
-private:
-  double _x;
-  double _y;
-  double _z;
+#include "MathUtils.hpp"
 
+class Vec3 {
 public:
+  double x;
+  double y;
+  double z;
+
   Vec3();
   Vec3(double x, double y, double z);
 
-  double getX() const;
-  double getY() const;
-  double getZ() const;
-
-  void setX(double x);
-  void setY(double y);
-  void setZ(double z);
+  void set(double x, double y, double z);
 
   double length() const;
-
   double lengthSquared() const;
-
   bool equals(const Vec3 &v) const;
 
+  static Vec3 random() {
+    return Vec3(random_double(), random_double(), random_double());
+  }
+
+  static Vec3 random(double min, double max) {
+    return Vec3(random_double(min, max), random_double(min, max),
+                random_double(min, max));
+  }
+
   Vec3 operator-() const;
-
   Vec3 &operator+=(const Vec3 &v);
-
   Vec3 &operator-=(const Vec3 &v);
-
   Vec3 &operator*=(const Vec3 &v);
-
   Vec3 &operator/=(const double t);
+
+  Vec3 normalize() const;
+  double dot(const Vec3 &v) const;
+  Vec3 cross(const Vec3 &v) const;
+
+  static Vec3 randomInUnitSphere();
+  static Vec3 randomUnit();
+  static Vec3 randomOnHemisphere(const Vec3 &normal);
 };
 
 inline Vec3 operator+(const Vec3 &u, const Vec3 &v) {
-  return Vec3(u.getX() + v.getX(), u.getY() + v.getY(), u.getZ() + v.getZ());
+  return Vec3(u.x + v.x, u.y + v.y, u.z + v.z);
 }
 
 inline Vec3 operator-(const Vec3 &u, const Vec3 &v) {
-  return Vec3(u.getX() - v.getX(), u.getY() - v.getY(), u.getZ() - v.getZ());
+  return Vec3(u.x - v.x, u.y - v.y, u.z - v.z);
 }
 
 inline Vec3 operator*(const Vec3 &u, const Vec3 &v) {
-  return Vec3(u.getX() * v.getX(), u.getY() * v.getY(), u.getZ() * v.getZ());
+  return Vec3(u.x * v.x, u.y * v.y, u.z * v.z);
 }
 
 inline Vec3 operator*(const Vec3 &v, const double t) {
-  return Vec3(v.getX() * t, v.getY() * t, v.getZ() * t);
+  return Vec3(v.x * t, v.y * t, v.z * t);
 }
 
 inline Vec3 operator*(const double t, const Vec3 &v) { return v * t; }
 
 inline Vec3 operator/(const Vec3 &v, const double t) { return v * (1 / t); }
 
-inline double dotProduct(const Vec3 &u, const Vec3 &v) {
-  return u.getX() * v.getX() + u.getY() * v.getY() + u.getZ() * v.getZ();
+inline Vec3 Vec3::normalize() const {
+  double l = length();
+
+  return Vec3(x / l, y / l, z / l);
 }
 
-inline Vec3 crossProduct(const Vec3 &u, const Vec3 &v) {
-  return Vec3(u.getY() * v.getZ() - u.getZ() * v.getY(),
-              u.getZ() * v.getX() - u.getX() * v.getZ(),
-              u.getX() * v.getY() - u.getY() * v.getX());
+inline double Vec3::dot(const Vec3 &v) const {
+  return x * v.x + y * v.y + z * v.z;
 }
 
-inline Vec3 unitVectorFrom(const Vec3 &v) { return v / v.length(); }
+inline Vec3 Vec3::cross(const Vec3 &v) const {
+  return Vec3(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
+}
+
+inline Vec3 Vec3::randomInUnitSphere() {
+  while (true) {
+    auto v = Vec3::random(-1, 1);
+
+    if (v.lengthSquared() < 1) {
+      return v;
+    }
+  }
+}
+
+inline Vec3 Vec3::randomUnit() { return randomInUnitSphere().normalize(); }
+
+inline Vec3 Vec3::randomOnHemisphere(const Vec3 &normal) {
+  Vec3 v = randomUnit();
+
+  return v.dot(normal) > 0 ? v : -v;
+}
 
 using Point3 = Vec3;
 using Color = Vec3;
--- src/math/Vec3.cpp
+++ src/math/Vec3.cpp
@@ -2,55 +2,51 @@
 
 #include <cmath>
 
-Vec3::Vec3() : _x(0), _y(0), _z(0) {}
-Vec3::Vec3(double x, double y, double z) : _x(x), _y(y), _z(z) {}
+Vec3::Vec3() : x(0), y(0), z(0) {}
+Vec3::Vec3(double x, double y, double z) : x(x), y(y), z(z) {}
 
-double Vec3::getX() const { return _x; }
-double Vec3::getY() const { return _y; }
-double Vec3::getZ() const { return _z; }
-
-void Vec3::setX(double x) { _x = x; }
-void Vec3::setY(double y) { _y = y; }
-void Vec3::setZ(double z) { _z = z; }
-
-double Vec3::length() const { return sqrt(lengthSquared()); }
-
-double Vec3::lengthSquared() const { return _x * _x + _y * _y + _z * _z; }
-
-bool Vec3::equals(const Vec3 &v) const {
-  return _x == v.getX() && _y == v.getY() && _z == v.getZ();
+void Vec3::set(double x, double y, double z) {
+  this->x = x;
+  this->y = y;
+  this->z = z;
 }
 
-Vec3 Vec3::operator-() const { return Vec3(-_x, -_y, -_z); }
+double Vec3::length() const { return sqrt(lengthSquared()); }
+double Vec3::lengthSquared() const { return x * x + y * y + z * z; }
+bool Vec3::equals(const Vec3 &v) const {
+  return x == v.x && y == v.y && z == v.z;
+}
+
+Vec3 Vec3::operator-() const { return Vec3(-x, -y, -z); }
 
 Vec3 &Vec3::operator+=(const Vec3 &v) {
-  _x += v.getX();
-  _y += v.getY();
-  _z += v.getZ();
+  x += v.x;
+  y += v.y;
+  z += v.z;
 
   return *this;
 }
 
 Vec3 &Vec3::operator-=(const Vec3 &v) {
-  _x -= v.getX();
-  _y -= v.getY();
-  _z -= v.getZ();
+  x -= v.x;
+  y -= v.y;
+  z -= v.z;
 
   return *this;
 }
 
 Vec3 &Vec3::operator*=(const Vec3 &v) {
-  _x *= v.getX();
-  _y *= v.getY();
-  _z *= v.getZ();
+  x *= v.x;
+  y *= v.y;
+  z *= v.z;
 
   return *this;
 }
 
 Vec3 &Vec3::operator/=(const double t) {
-  _x /= t;
-  _y /= t;
-  _z /= t;
+  x /= t;
+  y /= t;
+  z /= t;
 
   return *this;
 }
